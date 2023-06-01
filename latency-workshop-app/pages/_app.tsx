0dcadb4f07b3705e8facb04a520fba78e21498a2
Vyvienne Bautista <vbf.bautista@gmail.com>
2023-06-01 14:52:02 +0800

    Uploading remaining Module01 files

---
+++ latency-workshop-app/pages/_app.tsx
@@ -0,0 +1,20 @@
+import "@/styles/globals.css";
+import { ThemeProvider } from "@mui/material";
+import type { AppProps } from "next/app";
+import theme from "../styles/theme";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+
+// import '@/styles/globals.css'
+// import type { AppProps } from 'next/app'
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
